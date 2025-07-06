169a017366693d485d305739e493520b54f5137d
Ziwen Yuan <111393672+ZWYuan96@users.noreply.github.com>
2025-07-06 16:50:50 -0400

    allow fig output in compare_dist

--- Matlab/data_process/compare_dist.m
+++ Matlab/data_process/compare_dist.m
@@ -1,4 +1,4 @@
-function ax = compare_dist(datas, legends, bandw)
+function [ax, fig] = compare_dist(datas, legends, bandw)
 % this function is for plotting distributions 
 % Attentions:
 %            datas must have dimension M*N, where each column contains a
@@ -19,13 +19,13 @@ if nc ~= length(legends)
     error('please provide correct number of legend strings')
 end
 
-figure
-set(gcf, 'position', [1 1 800 500])
+fig = figure;
+set(gcf, 'position', [1 1 1600 600])
 hold on 
 for ii = 1:nc
     datai = datas(:, ii);
-    [f, x] = ksdensity(datai, 'Bandwidth', bandw);
-    plot(x, f, 'LineWidth', 3);
+    [f, x] = ksdensity(datai, 'Bandwidth', bandw, 'NumPoints', 500);
+    plot(x, f, 'LineWidth', 1.5);
 end
 legend(legends, 'Location', 'best');
 legend('boxoff');
@@ -33,3 +33,4 @@ legend('boxoff');
 ax = gca;
 ax.FontSize = 20;
 ax.LineWidth = 3;
+% ax.YLim = [0 1];
